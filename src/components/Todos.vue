82f225362be5d2e0a6b821a677ba26147f972caa
airton-silva <airtonsilvan89@gmail.com>
2021-07-21 11:12:29 -0300

    listagem de todos com seus usuarios e filtragem de completed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,20 +4,31 @@
             <h1>{{ msg }}</h1>
 
         <button class="btn btn-success" @click="getAll()">get Todos</button><br><br>
-                    <!-- {{ todos }} -->
+                    
         <div class="card col-md-8 offset-2" v-for="todo in todos" :key="todo.id">
-            <div class="card-body">
-              {{ users }}
+            <div class="card-body" v-if="todo.completed === true">
+                    <!-- {{ todo.id }} -->
+                <h3 class="card-title">Titulo : {{todo.title}}</h3>
                 <div v-for="user in users" :key="user.id">
                   <div  v-if="user.id ==todo.userId" class="alert alert-info" >                      
                        <span class="col-md-4"><strong>User/Autor: </strong>{{user.name}}</span>
-                       
-                  </div>
-                  
+                       <span class="col-md-4"><strong>Nome de Usuario: </strong>{{user.username}}</span>
+                       <span class="col-md-4"><strong>Email: </strong>{{user.email}}</span>
+                       <span class="col-md-4"><strong>Telefone: </strong>{{user.phone}}</span>
+                       <span class="col-md-4"><strong>Site: </strong>{{user.website}}</span>
+                       <h5>Endereço:</h5>
+                       <span class="col-md-4"><strong>Rua: </strong>{{user.address.street}}</span>
+                       <span class="col-md-4"><strong>Nº: </strong>{{user.address.suite}}</span>
+                       <span class="col-md-4"><strong>Nº: </strong>{{user.address.suite}}</span>
+                       <span class="col-md-4"><strong>Cidade: </strong>{{user.address.city}}</span>
+                       <span class="col-md-4"><strong>Cep: </strong>{{user.address.zipcode}}</span>
+                       <h5>Empresa:</h5>
+                       <span class="col-md-4"><strong>Nome: </strong>{{user.company.name}}</span>
+                       <span class="col-md-4"><strong>Slogan: </strong>{{user.company.catchPhrase}}</span>
+                       <span class="col-md-4"><strong>Ramo: </strong>{{user.company.bs}}</span>
+                  </div>                 
                 
                 </div>
-               
-                <h5 class="card-title">Titulo : {{todo.title}}</h5>
 
             </div>
         </div>
@@ -48,31 +59,38 @@ export default {
 
   methods: {
       getAll() {
-          axios.get(this.uriBase)
-            .then(result =>{
-                this.todos = result.data
-                this.getUserByTodosId (this.todos)
+
+          axios.all([
+            axios.get(this.uriBase),
+            axios.get(this.uriUser)
+          ]).then(axios.spread(( ResTodos, ResUser)=> {
+            this.todos = ResTodos.data
+            this.users = ResUser.data
+          }))
+          // axios.get(this.uriBase)
+          //   .then(result =>{
+          //       this.todos = result.data
+          //       this.getUserByTodosId (this.todos)
                
-            })
+          //   })
             // console.log(this.todos)
             
             
       },
 
-      getUserByTodosId (todos) {        
-          axios.get(this.uriUser)
-            .then(result =>{
-                this.users = result.data
-                this.users = this.users.filter(function(user) {
-                  return user.id == todos.userId;
+      // getUserByTodosId (todos) {   
+          
+      //     axios.get(this.uriUser)
+      //       .then(result =>{
+      //           this.users = result.data
+      //           this.users = this.users.filter(function(user) {
+      //             return user.id == todos.userId;
                  
-                });
-                //console.log(this.users)
-            })
-
-
+      //           });
+      //           console.log(this.users)
+      //       })
       
-      },
+      // },
 
   },
 
